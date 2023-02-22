022b0a95d2e127f079f7833c2b0d261903362386
kh953317 <60681154+kh953317@users.noreply.github.com>
2023-02-22 23:11:24 +0800

    Add files via upload

---
+++ ViewImages/ViewImages/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace ViewImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(729, 463);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 402);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(427, 60);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(408, 402);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "C:\\Users\\Snail\\Pictures\\[155]Still Still Stellar - Andromeda Wallpaper All [Remas" +
+    "tered]";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(427, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(408, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "C:\\Users\\Snail\\Pictures\\[155]Still Still Stellar - Andromeda Wallpaper All [Remas" +
+    "tered] - 複製";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(841, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(408, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "C:\\Users\\Snail\\Pictures\\[155]Still Still Stellar - Andromeda Wallpaper All [Remas" +
+    "tered] - 複製 (2)";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(841, 60);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(408, 402);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 463);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 21);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(427, 36);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 16);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(841, 36);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(77, 16);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(603, 488);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 25);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(611, 465);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 22);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(106, 35);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(45, 22);
+            this.numericUpDown2.TabIndex = 14;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(522, 35);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(45, 22);
+            this.numericUpDown3.TabIndex = 15;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(935, 35);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(45, 22);
+            this.numericUpDown4.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 536);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+    }
+}
+
---
+++ ViewImages/ViewImages/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViewImages
+{
+    public partial class Form1 : Form
+    {
+        private float X;//當前窗體的寬度
+        private float Y;//當前窗體的高度
+        bool isLoaded;  // 是否已設定各控制的尺寸資料到Tag屬性
+        //
+        string folder1 = "",folder2 = "", folder3 = "";
+        string[] files1, files2, files3;
+        string[,] files;
+        int[] now = { 0,0,0};
+        int[] allnum = { 0,0,0};
+        //
+        public Form1()
+        {
+            InitializeComponent();
+            isLoaded = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X = this.Width;//獲取窗體的寬度
+            Y = this.Height;//獲取窗體的高度
+            isLoaded = true;// 已設定各控制項的尺寸到Tag屬性中
+            SetTag(this);//調用方法
+        }
+        /// <summary>
+        /// 將控制項的寬，高，左邊距，頂邊距和字體大小暫存到tag屬性中
+        /// </summary>
+        /// <param name="cons">遞歸控制項中的控制項</param>
+        private void SetTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    SetTag(con);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked)
+                {
+                    if (now[0] <= 0) now[0] = allnum[0];
+                    try
+                    {
+                        pictureBox1.Image = new Bitmap(files1[now[0] - 1]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    now[0]--;
+                    numericUpDown2.Value = now[0] + 1;
+                }
+
+                if (checkBox2.Checked)
+                {
+                    if (now[1] <= 0) now[1] = allnum[1];
+                    try
+                    {
+                        pictureBox2.Image = new Bitmap(files2[now[1] - 1]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    now[1]--;
+                    numericUpDown3.Value = now[1] + 1;
+                }
+
+                if (checkBox3.Checked)
+                {
+                    if (now[2] <= 0) now[2] = allnum[2];
+                    try
+                    {
+                        pictureBox3.Image = new Bitmap(files3[now[2] - 1]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    now[2]--;
+                    numericUpDown4.Value = now[2] + 1;
+                }
+
+                GC.Collect();
+
+
+                //if (now[0] == 0) now[0] = allnum[0];
+                //if(now[1] == 0) now[1] = allnum[1];
+                //if (now[2] == 0) now[2] = allnum[2];
+
+                //now[0]--;
+                //now[1]--;
+                //now[2]--;
+
+                //pictureBox1.Image = new Bitmap(files1[now[0]]);
+                //pictureBox2.Image = new Bitmap(files2[now[0]]);
+                //pictureBox3.Image = new Bitmap(files3[now[0]]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked)
+                {
+                    if (now[0] >= allnum[0] - 1) now[0] = -1;
+                    try
+                    {
+                        pictureBox1.Image = new Bitmap(files1[now[0] + 1]);
+                    }
+                    catch(Exception ex)
+                    {
+                        
+                    }
+                    now[0]++;
+                    numericUpDown2.Value = now[0] + 1;
+                }
+
+                if (checkBox2.Checked)
+                {
+                    if (now[1] >= allnum[1] - 1) now[1] = -1;
+                    try
+                    {
+                        pictureBox2.Image = new Bitmap(files2[now[1] + 1]);
+                    }
+                    catch (Exception ex)
+                    {
+                        
+                    }
+
+                    now[1]++;
+                    numericUpDown3.Value = now[1] + 1;
+                }
+
+                if (checkBox3.Checked)
+                {
+                    if (now[2] >= allnum[2] - 1) now[2] = -1;
+                    try
+                    {
+                        pictureBox3.Image = new Bitmap(files3[now[2] + 1]);
+                    }
+                    catch (Exception ex)
+                    {
+                        
+                    }
+
+                    now[2]++;
+                    numericUpDown4.Value = now[2] + 1;
+                }
+
+                //if (now[0] == allnum[0]-1) now[0] = 0;
+                //if (now[1] == allnum[1] - 1) now[1] = 0;
+                //if (now[2] == allnum[2] -1) now[2] = 0;
+
+                //now[0]++;
+                //now[1]++;
+                //now[2]++;
+
+                //pictureBox1.Image = new Bitmap(files1[now[0]]);
+                //pictureBox2.Image = new Bitmap(files2[now[1]]);
+                //pictureBox3.Image = new Bitmap(files3[now[2]]);
+
+                GC.Collect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked && (int)numericUpDown1.Value != 0)
+            {
+                if((int)numericUpDown1.Value < allnum[0])
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            now[0] = (int)numericUpDown1.Value;
+            now[1] = (int)numericUpDown1.Value;
+            now[2] = (int)numericUpDown1.Value;
+
+        }
+
+        private void SetControls(float newx, float newy, Control cons)
+        {
+            if (isLoaded)
+            {
+                //遍歷窗體中的控制項，重新設置控制項的值
+                foreach (Control con in cons.Controls)
+                {
+                    string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//獲取控制項的Tag屬性值，並分割後存儲字元串數組
+                    float a = System.Convert.ToSingle(mytag[0]) * newx;//根據窗體縮放比例確定控制項的值，寬度
+                    con.Width = (int)a;//寬度
+                    a = System.Convert.ToSingle(mytag[1]) * newy;//高度
+                    con.Height = (int)(a);
+                    a = System.Convert.ToSingle(mytag[2]) * newx;//左邊距離
+                    con.Left = (int)(a);
+                    a = System.Convert.ToSingle(mytag[3]) * newy;//上邊緣距離
+                    con.Top = (int)(a);
+                    //Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字體大小
+                    //con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                    if (con.Controls.Count > 0)
+                    {
+                        SetControls(newx, newy, con);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            float xx = this.Width / X;
+            float yy = this.Height / Y;
+            SetControls(xx,yy,this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked) folder1 = @textBox1.Text;
+            if(checkBox2.Checked) folder2 = @textBox2.Text;
+            if (checkBox3.Checked)  folder3 = @textBox3.Text;
+
+            //if(!Directory.Exists(folder1) || !Directory.Exists(folder2) || !Directory.Exists(folder3))
+            //{
+            //    MessageBox.Show("一個以上資料夾不存在");
+            //    return;
+            //}
+            allnum = new int[] { 0,0,0};
+            try
+            {
+                
+                if (checkBox1.Checked)
+                {
+                    folder1 = @textBox1.Text;
+                    files1 = Directory.GetFiles(folder1);
+                    pictureBox1.Image = new Bitmap(files1[0]);
+                    allnum[0] = files1.Length;
+                    numericUpDown2.Value = 1;
+                }
+
+                if(checkBox2.Checked)
+                {
+                    folder2 = @textBox2.Text;
+                    files2 = Directory.GetFiles(folder2);
+                    pictureBox2.Image = new Bitmap(files2[0]);
+                    allnum[1] = files2.Length;
+                    numericUpDown3.Value = 1;
+                }
+                
+                if(checkBox3.Checked)
+                {
+                    folder3 = @textBox3.Text;
+                    files3 = Directory.GetFiles(folder3);
+                    pictureBox3.Image = new Bitmap(files3[0]);
+                    allnum[2] = files3.Length;
+                    numericUpDown4.Value = 1;
+                }
+
+                now = new int[] { 0,0,0};
+                //allnum = new int[] { files1.Length, files2.Length, files3.Length};
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
